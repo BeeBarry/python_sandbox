6cf4fae4115887f9c341043bc396011d126c31b4
BeeBarry <barry.namdari@gmail.com>
2025-10-08 13:00:41 +0200

    New section: OOP - Object Oriented Programming

---
+++ python_fundamentals/oop/main.py
@@ -0,0 +1,10 @@
+from whale import Whale
+
+whale1 = Whale('Willy', 'Humpback whale', 3 )
+whale2 = Whale('Astrid', 'Blue whale', 62)
+whale3 = Whale ('Kim', 'Dolphin', 15)
+
+whale2.sleep()
+whale3.sing()
+whale1.birthday()
+
