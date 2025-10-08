b809862821db1d4c87524a4fa6056013d22c880f
BeeBarry <barry.namdari@gmail.com>
2025-10-08 20:53:09 +0200

    Refactor: Constants for often modified variables ( movement directions)

--- python_games/snake_game/snake.py
+++ python_games/snake_game/snake.py
@@ -1,15 +1,17 @@
 from turtle import Turtle
 
-
-starting_positions = [(0,0), (-20,0), (-40,0)]
+# Contstanter för att centralsera variablerna jag kommer skruva mycket på.
+STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
 
 class Snake:
     def __init__(self):
         self.segments= []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for position in starting_positions:
+        for position in STARTING_POSITIONS:
                 new_segment = Turtle('square')
                 new_segment.color('white')
                 new_segment.penup()
@@ -17,10 +19,21 @@ class Snake:
                 self.segments.append(new_segment) # added "self" so it can self-append
 
     def move(self):
-        for seg_num in range(len(self.segments) - 1, 0,
-                             -1):  # (start,stop,step implicit logic). 2 is the last segment and 0 the first, going in reverse order.
+        for seg_num in range(len(self.segments) - 1, 0,-1):  # (start,stop,step implicit logic). 2 is the last segment and 0 the first, going in reverse order.
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
 
+    def up(self):
+        self.head.setheading(90)
+
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
