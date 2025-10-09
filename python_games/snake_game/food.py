e2825d31240231de29b20b894390fdeaad97abe8
BeeBarry <barry.namdari@gmail.com>
2025-10-09 11:56:53 +0200

    Add: food klassen

---
+++ python_games/snake_game/food.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup() # För att den inte skall rita något.
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color('purple')
+        self.speed('fastest') # Hindra "ghosting" när den skapas centralt och sedan flyttas ut.
+        random_x = random.randint(-280,280)
+        random_y = random.randint(-280,280)
+        self.goto(random_x, random_y)
